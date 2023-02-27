ecf3ee7ec07fc4c833d50bd3bd9cc26bef8e29f9
Thanh Ngoc <tngocnp@pm.me>
2023-02-27 14:10:27 +0700

    Routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    component: ProductComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const RoutingComponent = [ProductComponent];
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
-
+  Products = {
+    ProductId: 123,
+    ProductName: 'Atomic Habits',
+    ProductPrice: 300,
+    ProductImage: '../assets/atomic-habits.jpg',
+  };
 }
