f2304654d2d8bac44d472b7a3110dee59cd3e56e
Thanh Ngoc <tngocnp@pm.me>
2023-02-27 15:13:41 +0700

    Another way for Routing, illustrating duplicate data problem.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListProductComponent } from './list-product/list-product.component';
+import { ListProduct2Component } from './list-product2/list-product2.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'list-product',
     component: ListProductComponent,
   },
+  {
+    path: 'list-product2',
+    component: ListProduct2Component,
+  },
 ];
 
 @NgModule({
@@ -20,4 +25,8 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 
-export const RoutingComponent = [ProductComponent, ListProductComponent];
+export const RoutingComponent = [
+  ProductComponent,
+  ListProductComponent,
+  ListProduct2Component,
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ListProductComponent } from './list-product/list-product.component';
+import { ListProduct2Component } from './list-product2/list-product2.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, RoutingComponent, ListProductComponent],
+  declarations: [AppComponent, ProductComponent, RoutingComponent, ListProductComponent, ListProduct2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
