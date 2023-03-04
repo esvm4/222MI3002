e75e8365a6bc0a6ba1b7812bb25f34fb6d0326f5
Thanh Ngoc <tngocnp@pm.me>
2023-03-04 12:17:22 +0700

    Add index for module01.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { ListProductComponent } from './list-product/list-product.component';
 import { ListProduct2Component } from './list-product2/list-product2.component';
 import { ProductComponent } from './product/product.component';
 import { MyComponentComponent } from './module01/my-component/my-component.component';
+import { BindingPropertyComponentComponent } from './module01/binding-property-component/binding-property-component.component';
+import { IndexComponent } from './module01/index/index.component';
 
 const routes: Routes = [
   {
@@ -24,8 +26,16 @@ const routes: Routes = [
     component: ListCatalogueComponent,
   },
   {
+    path: 'index-component',
+    component: IndexComponent,
+  },
+  {
     path: 'my-component',
     component: MyComponentComponent,
+  },
+  {
+    path: 'binding-property-component',
+    component: BindingPropertyComponentComponent,
   },
 ];
 
@@ -40,5 +50,7 @@ export const RoutingComponent = [
   ListProductComponent,
   ListProduct2Component,
   ListCatalogueComponent,
+  IndexComponent,
   MyComponentComponent,
+  BindingPropertyComponentComponent,
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListProductComponent } from './list-product/list-product.component';
 import { ListProduct2Component } from './list-product2/list-product2.component';
 import { ListCatalogueComponent } from './list-catalogue/list-catalogue.component';
 import { MyComponentComponent } from './module01/my-component/my-component.component';
+import { BindingPropertyComponentComponent } from './module01/binding-property-component/binding-property-component.component';
+import { IndexComponent } from './module01/index/index.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { MyComponentComponent } from './module01/my-component/my-component.compo
     ListProduct2Component,
     ListCatalogueComponent,
     MyComponentComponent,
+    BindingPropertyComponentComponent,
+    IndexComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
