0ae5c2b7b2e2c15dc68163fcce1cd4724817b967
Thanh Ngoc <tngocnp@pm.me>
2023-03-08 22:15:55 +0700

    Add blank lunar-year template, in case Ng crash.

---
+++ src/app/module01/lunar-year/lunar-year.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lunar-year',
+  templateUrl: './lunar-year.component.html',
+  styleUrls: ['./lunar-year.component.css'],
+})
+export class LunarYearComponent {
+  constructor() {}
+}
